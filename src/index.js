00e02a846ae77a44ac53bb40455b479e9e1cf02e
François Beuvens <francois.beuvens@gmail.com>
2016-02-27 14:49:08 +0100

    support any type of promise

--- src/index.js
+++ src/index.js
@@ -2,7 +2,7 @@ export const start = actionName => `${actionName}_START`;
 export const end = actionName => `${actionName}_END`;
 
 export const promiseLifecycleMiddleware = store => next => action => {
-  if (action.payload instanceof Promise) {
+  if (action.payload && typeof action.payload.then === 'function') {
     store.dispatch({ ...action, type: start(action.type), payload: null });
     return next({ ...action, type: end(action.type) });
   }
